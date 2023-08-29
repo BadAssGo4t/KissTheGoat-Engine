a7748ccc1b1b3d117cdf651c2e1c4b3fde052a2d
Estanislao <estanislao.sb@gmail.com>
2023-08-29 12:22:37 -0300

    buffer
    created a  buffer to create the triangle cordinates

--- Engine/Window.cpp
+++ Engine/Window.cpp
@@ -23,17 +23,33 @@ int CreateWindow(int width, int height, const char* windowName)
     if (glewInit() != GLEW_OK) // GLEW INIT
         cout << '\n' << "GLEW INIT ERROR";
 
+    cout << glGetString(GL_VERSION);
+
+    float positionsTriangle[6] = 
+    {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f,
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER,6* sizeof(float), positionsTriangle, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // create vertex struct
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements()
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
